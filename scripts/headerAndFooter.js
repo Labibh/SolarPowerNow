76f4995671bcb01153b2a665e907d58e69418c13
Labib Haque <labibh@gmail.com>
2022-11-13 21:32:46 -0500

    Bug fix: footer and header not loading

--- scripts/headerAndFooter.js
+++ scripts/headerAndFooter.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-    $("#header").load("./header.html");
-    $("#footer").load("./footer.html");
+    $("#header").load("https://labibh.github.io/SolarPowerNow/header.html");
+    $("#footer").load("https://labibh.github.io/SolarPowerNow/footer.html");
 });
