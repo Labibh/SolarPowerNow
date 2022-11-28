a4f74f958e90773fc6cbb8391c7a31aaff93a54f
Labib Haque <labibh@gmail.com>
2022-11-28 15:44:32 -0500

    Dashboard page complete

---
+++ scripts/dashboard.js
@@ -0,0 +1,205 @@
+var svgMapDataGPD = {
+    data: {
+        solarCapacity: {
+            name: 'Cumulative Solar PV Capacity',
+            format: '{0} MW',
+            thousandSeparator: ',',
+            thresholdMax: 310000,
+            thresholdMin: 3500
+        },
+        solarGrowth: {
+            name: 'Solar Capacity Growth 2021',
+            format: '{0} MW',
+            thousandSeparator: ',',
+        },
+        gdp: {
+            name: 'GDP per capita',
+            format: '{0} USD',
+            thousandSeparator: ','
+        },
+        population: {
+            name: 'Population',
+            thousandSeparator: ','
+        },
+
+        landArea: {
+            name: 'Area',
+            format: '{0} km<sup>2</sup>',
+            thousandSeparator: ','
+        }
+    },
+    applyData: 'solarCapacity',
+    values: {
+        CA: {
+            link: 'https://pt.wikipedia.org/wiki/CA',
+            linkTarget: '_blank',
+            gdp: 45077,
+            population: 37279841,
+            landArea: 9984670,
+            solarCapacity: 3630,
+            solarGrowth: 305
+        },
+        CN: {
+            link: 'https://pt.wikipedia.org/wiki/CN',
+            linkTarget: '_blank',
+            gdp: 8643,
+            population: 1395098219,
+            landArea: 9640821,
+            solarCapacity: 306973,
+            solarGrowth: 53009
+        },
+        US: {
+            link: 'https://pt.wikipedia.org/wiki/US',
+            linkTarget: '_blank',
+            gdp: 59501,
+            population: 328150370,
+            landArea: 9833517,
+            solarCapacity: 95209,
+            solarGrowth: 19647
+        },
+        IN: {
+            link: 'https://pt.wikipedia.org/wiki/IN',
+            linkTarget: '_blank',
+            gdp: 1983,
+            population: 1339374357,
+            landArea: 3287240,
+            solarCapacity: 56951,
+            solarGrowth: 10299
+        },
+        BR: {
+            link: 'https://pt.wikipedia.org/wiki/BR',
+            linkTarget: '_blank',
+            gdp: 9895,
+            population: 209828546,
+            landArea: 8515767,
+            solarCapacity: 13055,
+            solarGrowth: 5176
+        },
+        DE: {
+            link: 'https://pt.wikipedia.org/wiki/DE',
+            linkTarget: '_blank',
+            gdp: 44550,
+            population: 82793800,
+            landArea: 357168,
+            solarCapacity: 58461,
+            solarGrowth: 4740
+        },
+        JP: {
+            link: 'https://pt.wikipedia.org/wiki/JP',
+            linkTarget: '_blank',
+            gdp: 38440,
+            population: 126440000,
+            landArea: 377944,
+            solarCapacity: 74191,
+            solarGrowth: 4427
+        },
+        ES: {
+            link: 'https://pt.wikipedia.org/wiki/ES',
+            linkTarget: '_blank',
+            gdp: 28359,
+            population: 46659302,
+            landArea: 505990,
+            solarCapacity: 15952,
+            solarGrowth: 3363
+        },
+        NL: {
+            link: 'https://pt.wikipedia.org/wiki/NL',
+            linkTarget: '_blank',
+            gdp: 48346,
+            population: 17269908,
+            landArea: 41526,
+            solarCapacity: 14249,
+            solarGrowth: 3229
+        },
+        FR: {
+            link: 'https://pt.wikipedia.org/wiki/FR',
+            linkTarget: '_blank',
+            gdp: 39869,
+            population: 67348000,
+            landArea: 543965,
+            solarCapacity: 14718,
+            solarGrowth: 2687
+        },
+        PL: {
+            link: 'https://pt.wikipedia.org/wiki/PL',
+            linkTarget: '_blank',
+            gdp: 13822,
+            population: 38433600,
+            landArea: 312685,
+            solarCapacity: 6257,
+            solarGrowth: 2302
+        },
+        TW: {
+            link: 'https://pt.wikipedia.org/wiki/TW',
+            linkTarget: '_blank',
+            gdp: 24577,
+            population: 23577271,
+            landArea: 36197,
+            solarCapacity: 7700,
+            solarGrowth: 1883
+        },
+        MX: {
+            link: 'https://pt.wikipedia.org/wiki/MX',
+            linkTarget: '_blank',
+            gdp: 9304,
+            population: 124737789,
+            landArea: 1967138,
+            solarCapacity: 7040,
+            solarGrowth: 1877
+        },
+        AU: {
+            link: 'https://pt.wikipedia.org/wiki/AU',
+            linkTarget: '_blank',
+            gdp: 55707,
+            population: 25119327,
+            landArea: 7692024,
+            solarCapacity: 19076,
+            solarGrowth: 1732
+        },
+        CL: {
+            link: 'https://pt.wikipedia.org/wiki/CL',
+            linkTarget: '_blank',
+            gdp: 15070,
+            population: 17373831,
+            landArea: 756096,
+            solarCapacity: 4468,
+            solarGrowth: 1263
+        },
+        IT: {
+            link: 'https://pt.wikipedia.org/wiki/IT',
+            linkTarget: '_blank',
+            gdp: 31984,
+            population: 60404843,
+            landArea: 301308,
+            solarCapacity: 22698,
+            solarGrowth: 1098
+        },
+        KR: {
+            link: 'https://pt.wikipedia.org/wiki/KR',
+            linkTarget: '_blank',
+            gdp: 29891,
+            population: 51635256,
+            landArea: 100210,
+            solarCapacity: 18161,
+            solarGrowth: 3586
+        },
+        VN: {
+            link: 'https://pt.wikipedia.org/wiki/VN',
+            linkTarget: '_blank',
+            gdp: 2354,
+            population: 94660000,
+            landArea: 331212,
+            solarCapacity: 16660,
+            solarGrowth: 156
+        },
+        GB: {
+            link: 'https://pt.wikipedia.org/wiki/GB',
+            linkTarget: '_blank',
+            gdp: 39735,
+            population: 66040229,
+            landArea: 242910,
+            solarCapacity: 13689,
+            solarGrowth: 126
+        }
+    }
+};
--- scripts/headerAndFooter.js
+++ scripts/headerAndFooter.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    //$("#header").load("./header.html");
-    //$("#footer").load("./footer.html");
+    // $("#header").load("./header.html");
+    // $("#footer").load("./footer.html");
     $("#header").load("https://labibh.github.io/SolarPowerNow/header.html");
     $("#footer").load("https://labibh.github.io/SolarPowerNow/footer.html");
 });
